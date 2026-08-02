cdf061503a4c19aa01b20aa22fc4ff4d1605aeaa
kandesaiteja49 <kandesaiteja49@gmail.com>
2026-08-02 23:00:14 +0530

    updated commit

--- src/main/java/com/taskmaster/dto/AuthResponse.java
+++ src/main/java/com/taskmaster/dto/AuthResponse.java
@@ -6,6 +6,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+// This class represents the response returned after a successful authentication, containing the access token and its type.
 public class AuthResponse {
     private String accessToken;
     private String tokenType = "Bearer";
